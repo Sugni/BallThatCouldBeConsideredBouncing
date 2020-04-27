33c4bc6f425146289c83a5f4f09f1bbef060e305
Ingus Andris Pērkons <slaimy333@gmail.com>
2020-04-27 16:43:55 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallThatCouldBeConsideredBouncing
+{
+    public partial class Form1 : Form
+    {
+
+        Timer mainTime = null;
+
+        int horVel = 0;
+        int verVel = 0;
+        int ballStep = 1;
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMainTimer();
+            InitializeBouncerMovement();
+        }
+
+        private void InitializeMainTimer()
+        {
+            mainTime = new Timer();
+            mainTime.Interval = 10;
+            mainTime.Tick += new EventHandler(MainTime_Tick);
+            mainTime.Start();
+        }
+
+        private void InitializeBouncerMovement()
+        {
+            verVel = ballStep;
+            horVel = ballStep;
+        }
+
+        private void MainTime_Tick(object sender, EventArgs e)
+        {
+            MoveBall();
+            ChangeDirection();
+        }
+
+        private void MoveBall()
+        {
+            if (pboxBouncer.Top + verVel > ClientRectangle.Height - pboxBouncer.Height)
+            {
+                pboxBouncer.Top = ClientRectangle.Height - pboxBouncer.Height;
+            }
+            else if(pboxBouncer.Top + verVel < 0)
+            {
+                pboxBouncer.Top = 0;
+            }
+            else
+            {
+                pboxBouncer.Top += verVel;
+            }
+            if (pboxBouncer.Left + horVel > ClientRectangle.Width - pboxBouncer.Width)
+            {
+                pboxBouncer.Left = ClientRectangle.Width - pboxBouncer.Width;
+            }
+            else if (pboxBouncer.Left + horVel < 0)
+            {
+                pboxBouncer.Left = 0;
+            }
+            else
+            {
+                pboxBouncer.Left += horVel;
+            }
+        }
+
+        private void ChangeDirection()
+        {
+            if(pboxBouncer.Top == ClientRectangle.Height - pboxBouncer.Height || pboxBouncer.Top == 0)
+            {
+                verVel = -verVel;
+                pboxBouncer.BackColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+            }
+            if(pboxBouncer.Left == ClientRectangle.Width - pboxBouncer.Width || pboxBouncer.Left == 0)
+            {
+                horVel = -horVel;
+                pboxBouncer.BackColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+            }
+        }
+    }
+
+}
